92e97d4edd74fdb4493582fc5c3a5d4d6e27a8ef
Alek Evans <github@alekeagle.com>
2025-03-14 18:01:51 -0800

    fard

--- gif-script.sh
+++ gif-script.sh
@@ -3,7 +3,9 @@ INPUT_FILE=$1
 
 OUTPUT_FILE=$2
 
-bruh=1
-ffmpeg -i $INPUT_FILE -i alpha.png -filter_complex "[0:v][1:v]alphamerge,split[v0][v1];[v0]palettegen[p];[v1][p]paletteuse" "$OUTPUT_FILE-original.gif"
-ffmpeg -i $INPUT_FILE -i alpha.png -filter_complex "[0:v][1:v]alphamerge,scale=256:256,split[v0][v1];[v0]palettegen[p];[v1][p]paletteuse" "$OUTPUT_FILE-256x256.gif"
-ffmpeg -i $INPUT_FILE -i alpha.png -filter_complex "[0:v][1:v]alphamerge,scale=71:71,split[v0][v1];[v0]palettegen[p];[v1][p]paletteuse" "$OUTPUT_FILE-71x71.gif"
+# Get current directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+ffmpeg -i $INPUT_FILE -i $DIR/alpha.png -filter_complex "[0:v][1:v]alphamerge,split[v0][v1];[v0]palettegen[p];[v1][p]paletteuse" "$OUTPUT_FILE-original.gif"
+ffmpeg -i $INPUT_FILE -i $DIR/alpha.png -filter_complex "[0:v][1:v]alphamerge,scale=256:256,split[v0][v1];[v0]palettegen[p];[v1][p]paletteuse" "$OUTPUT_FILE-256x256.gif"
+ffmpeg -i $INPUT_FILE -i $DIR/alpha.png -filter_complex "[0:v][1:v]alphamerge,scale=71:71,split[v0][v1];[v0]palettegen[p];[v1][p]paletteuse" "$OUTPUT_FILE-71x71.gif"

711a1a2ba71a8c46c413b843e5511ff24e2a657d
Alek Evans <github@alekeagle.com>
2026-03-04 21:31:16 -0900

    allow generator.sh to be symlinked and still properly work

--- generator.sh
+++ generator.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get current directory of the script, following any/all symlinks if necessary
+DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}" )" )" >/dev/null 2>&1 && pwd )"
+
 # Store all options in an array to be used later
 while [[ "$1" == --* ]]; do
   OPTIONS+=("$1")
@@ -11,10 +14,10 @@ INPUT_TEXTURE=$1
 OUTPUT_LOCATION="output/$2"
 
 # Make a directory to store our output
-mkdir -p $OUTPUT_LOCATION
+mkdir -p "$OUTPUT_LOCATION"
 
 # Generate our orb video file
-blender -y -b sphere.blend -- --texture $INPUT_TEXTURE --output $OUTPUT_LOCATION/orb.mp4
+blender -y -b "$DIR/sphere.blend" -- --texture "$INPUT_TEXTURE" --output "$OUTPUT_LOCATION/orb.mp4"
 
 # Generate our gifs and pass the options we stored in the array
-./gif-script.sh "${OPTIONS[@]}" $OUTPUT_LOCATION/orb.mp4 $OUTPUT_LOCATION/orb
+"$DIR/gif-script.sh" "${OPTIONS[@]}" "$OUTPUT_LOCATION/orb.mp4" "$OUTPUT_LOCATION/orb"
